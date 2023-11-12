a5c728f33da419828c381c7287aee99014b237a7
Landon Bouma <landonb@users.noreply.github.com>
2023-11-12 04:57:28 -0600

    Update: Resurrect <pkg>.__version__ for poetry-dynamic-versioning
    - Not sure why Click deprecated __version__, perhaps to avoid
      needing to maintain it.
    
    - Resurrecting because poetry-dynamic-versioning will maintain
      it at build-time, and because EAPP's CI workflow expects the
      <pkg>.__version__ for the smoke test.

--- src/click_hotoffthehamster/__init__.py
+++ src/click_hotoffthehamster/__init__.py
@@ -69,6 +69,9 @@ from .utils import get_binary_stream as get_binary_stream
 from .utils import get_text_stream as get_text_stream
 from .utils import open_file as open_file
 
+# As managed by poetry-dynamic-versionion.
+__version__ = ""
+
 
 def __getattr__(name: str) -> object:
     import warnings
